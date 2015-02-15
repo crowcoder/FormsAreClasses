782c6c8407a8eff4d632e70df268c0f7d08f4696
crowcoder <crowcoder@gmail.com>
2015-02-15 15:32:50 -0500

    Initial commit

---
+++ FormsClasses/Box.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FormsClasses
+{
+    partial class Box
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWidgetCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWidgetCount
+            // 
+            this.txtWidgetCount.Location = new System.Drawing.Point(55, 37);
+            this.txtWidgetCount.Name = "txtWidgetCount";
+            this.txtWidgetCount.Size = new System.Drawing.Size(151, 20);
+            this.txtWidgetCount.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the number of Widgets:";
+            // 
+            // Box
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 157);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWidgetCount);
+            this.Name = "Box";
+            this.Text = "Box Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtWidgetCount;
+    }
+}
+
---
+++ FormsClasses/BoxInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsClasses
+{
+    public partial class BoxInfo : Form
+    {
+        Box myBox;
+
+        public BoxInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myBox = new Box();
+            myBox.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = myBox.txtWidgetCount.Text;
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("You have to make a box first.");
+            }
+        }
+    }
+}
